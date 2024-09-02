d6d56cffca04d8a7b8dc0b43e19d027cf64ea86b
DSRN0000 <139203708+DSRN0000@users.noreply.github.com>
2024-09-02 12:36:53 +0900

    Title: 배열의 특정 위치 찾기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240902/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -0,0 +1,17 @@
+arr = list(map(int, input().split(" ")))
+n = len(arr)
+
+sum = 0
+avg = 0
+cnt = 0
+
+for i in range(n):
+    if (i + 1) % 2 == 0:
+        sum += arr[i]
+    if (i + 1) % 3 == 0:
+        avg += arr[i]
+        cnt += 1
+        
+avg /= cnt
+
+print(sum, avg)
