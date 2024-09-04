03552b61438ab08769bf2114f19f9a50cdb9867f
DSRN0000 <139203708+DSRN0000@users.noreply.github.com>
2024-09-04 14:10:52 +0900

    Title: 십의 자리 숫자, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240904/십의 자리 숫자/number-of-tens-digit.py
@@ -0,0 +1,10 @@
+# 0 -> 10의 자리수 몇개인지 출력
+arr = [0] * 10
+num_arr = list(map(int, input().split()))
+
+for i in num_arr:
+    num = i // 10
+    arr[num] += 1
+
+for j in range(1, 10):
+    print(j, "-", arr[j])
