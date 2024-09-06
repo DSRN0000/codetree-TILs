b384104a37d51392bab3f9b0f026104e69e02849
DSRN0000 <139203708+DSRN0000@users.noreply.github.com>
2024-09-06 17:06:55 +0900

    Title: 나눗셈의 나머지, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240906/나눗셈의 나머지/remainder-of-division.py
@@ -0,0 +1,14 @@
+# a에 a//b 계속 저장, a -> 1이하될때까지
+# 나머지 ** 횟수 -> 출력
+
+arr = [0] * 10
+a, b = map(int, input().split())
+result = 0
+
+while(a > 1):
+    a = a // b
+    arr[a % b] += 1
+
+for i in range(10):
+    result += arr[i] ** 2 
+print(result)
