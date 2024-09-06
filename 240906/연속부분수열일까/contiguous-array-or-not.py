db472923eab9c2ac80172fffd733a1e9b0029fb2
DSRN0000 <139203708+DSRN0000@users.noreply.github.com>
2024-09-06 19:44:07 +0900

    Title: 연속부분수열일까, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240906/연속부분수열일까/contiguous-array-or-not.py
+++ 240906/연속부분수열일까/contiguous-array-or-not.py
@@ -3,19 +3,18 @@ a_arr = list(map(int, input().split()))
 b_arr = list(map(int, input().split()))
 cnt = 0
 
-for i in range(b):
-    if b_arr[i] in a_arr:
-        num = a_arr.index(b_arr[i])
-        for j in range(num, a):
-            k = i
-            if a_arr[j] == b_arr[k] and k <= b:
-                cnt += 1
-                k += 1
-            else:
-                break
-    if cnt < 2:
-        cnt = 0
-if cnt >= 2:
-    print("Yes")
-if cnt < 2:
-    print("No")
+for i in range(a):
+    success = True
+
+    for j in range(b):
+        if i + j >= a:
+            success = False
+            break
+
+        if a_arr[i + j] != b_arr[j]:
+            success = False
+            break
+    if success:
+        print("Yes")
+        sys.exit()
+print("No")
