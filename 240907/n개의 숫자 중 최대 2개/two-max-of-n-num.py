5fb0637ca7ba39b404fbcb9f6cc909f13095b327
DSRN0000 <139203708+DSRN0000@users.noreply.github.com>
2024-09-07 03:26:15 +0900

    Title: n개의 숫자 중 최대 2개, Time: 136ms, Memory: 73MB, Status: Passed - CodeTree

---
+++ 240907/n개의 숫자 중 최대 2개/two-max-of-n-num.py
@@ -0,0 +1,10 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+for i in range(n-1):
+    for j in range(i + 1, n):
+        if arr[i] < arr[j]:
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+print(arr[0], arr[1])
