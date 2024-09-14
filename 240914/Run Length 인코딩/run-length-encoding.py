470c5903211a36133a0fe358d79f95c08d5d190f
DSRN0000 <139203708+DSRN0000@users.noreply.github.com>
2024-09-14 18:15:11 +0900

    Title: Run Length 인코딩, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240914/Run Length 인코딩/run-length-encoding.py
+++ 240914/Run Length 인코딩/run-length-encoding.py
@@ -3,18 +3,18 @@ len_a = len(a)
 arr = []
 arr_answer = []
 
-for i in range(len_a - 1):
+for i in range(len_a):
     if len(arr) == 0:
         arr.append(a[i])
 
-    if a[i + 1] in arr:
+    if i < len_a - 1 and a[i + 1] in arr:
         arr.append(a[i + 1])
 
-    if a[i + 1] not in arr or i == len_a - 2:
-        arr_answer.append(arr[0])
-        arr_answer.append(len(arr))
+    if i == len_a - 1 or a[i + 1] not in arr:  # 마지막 문자이거나 다음 문자가 다를 때
+        arr_answer.append(arr[0])  # 현재 문자 추가
+        arr_answer.append(len(arr))  # 연속된 문자 수 추가
         arr.clear()
 
 print(len(arr_answer))
 for j in arr_answer:
-    print(j, end= "")
+    print(j, end='')
