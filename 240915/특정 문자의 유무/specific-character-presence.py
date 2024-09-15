5839f5128095b948c5b3b68937bf095ca2b9a87b
DSRN0000 <139203708+DSRN0000@users.noreply.github.com>
2024-09-15 15:04:27 +0900

    Title: 특정 문자의 유무, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240915/특정 문자의 유무/specific-character-presence.py
@@ -0,0 +1,10 @@
+a = input()
+if "ee" in a:
+    print("Yes", end =" ")
+if "ee" not in a:
+    print("No", end =" ")
+
+if "ab" in a:
+    print("Yes", end =" ")
+if "ab" not in a:
+    print("No", end =" ")
