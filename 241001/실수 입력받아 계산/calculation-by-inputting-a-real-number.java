95fef1ddc094c662eea2fe37b4e1fce59a9b7145
DSRN0000 <139203708+DSRN0000@users.noreply.github.com>
2024-10-01 21:41:55 +0900

    Title: 실수 입력받아 계산, Time: 150ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241001/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc =  new Scanner(System.in);
+
+        double a, b, c;
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        c = a + b;
+
+        System.out.printf("%.2f", c);
+        
+    }
+}
