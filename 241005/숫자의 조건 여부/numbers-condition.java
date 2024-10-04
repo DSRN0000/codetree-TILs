ca36f116535b72f28ee2f32048cb12ee3025b010
DSRN0000 <139203708+DSRN0000@users.noreply.github.com>
2024-10-05 01:30:27 +0900

    Title: 숫자의 조건 여부, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241005/숫자의 조건 여부/numbers-condition.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a;
+
+        a = sc.nextInt();
+        if (113 <= a) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
