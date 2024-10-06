791cba584b85dbb2d28dabe9ebe490389959a0f4
DSRN0000 <139203708+DSRN0000@users.noreply.github.com>
2024-10-06 14:56:42 +0900

    Title: 알파벳 출력하기 2, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241006/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = 'A';
+
+        for (int i = n; i > 0; i--) {
+
+            for (int j = 0; j < n - i; j ++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 0; j < i; j++) {
+                if ((int)'Z' < x) {
+                    x = (int) 'A';
+                }
+                System.out.print((char) x + " ");
+                x++;
+            }
+            System.out.println();
+        }
+    }
+}
