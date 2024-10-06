afe625bfed72e258efa9019458bee598a9468013
DSRN0000 <139203708+DSRN0000@users.noreply.github.com>
2024-10-07 02:17:38 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241007/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[100];
+        int cnt = 0;
+
+        for (int i = 0; i < 100; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] == 0) {
+                break;
+            }
+            cnt++;
+        }
+        System.out.println(arr[cnt - 1] + arr[cnt - 2] + arr[cnt - 3]);
+    }
+}
