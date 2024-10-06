776876b20503a45288f8103573835fa5b06d509c
DSRN0000 <139203708+DSRN0000@users.noreply.github.com>
2024-10-07 00:53:59 +0900

    Title: 10개 입력받기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241007/10개 입력받기/receive-10-inputs.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] num = new int[10];
+        int cnt = 0;
+        int sum = 0;
+        double avg = 0;
+
+        for (int i = 0; i < 10; i++) {
+            num[i] = sc.nextInt();
+            if (num[i] == 0)  {
+                break;
+            }
+            sum += num[i]; 
+            cnt++;
+        }
+
+        avg = sum / cnt;
+
+        System.out.printf("%d %.1f", sum, avg);
+    }
+}
