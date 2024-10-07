9a0d08c23cfecf10bb8783b019db2ccc7750c3e5
DSRN0000 <139203708+DSRN0000@users.noreply.github.com>
2024-10-07 15:12:35 +0900

    Title: 두 개의 격자 비교하기, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241007/두 개의 격자 비교하기/compare-two-grid.java
+++ 241007/두 개의 격자 비교하기/compare-two-grid.java
@@ -26,7 +26,7 @@ public class Main {
                 if (arr1[i][j] == arr2[i][j]) {
                     arr3[i][j] = 0;
                 }
-                else { 
+                if (arr1[i][j] != arr2[i][j]) {
                     arr3[i][j] = 1;
                 }
             }
