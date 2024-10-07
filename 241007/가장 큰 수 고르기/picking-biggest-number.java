24fba563c11f197a310f197398b2369577dfe6a0
DSRN0000 <139203708+DSRN0000@users.noreply.github.com>
2024-10-07 11:32:01 +0900

    Title: 가장 큰 수 고르기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241007/가장 큰 수 고르기/picking-biggest-number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=  new Scanner(System.in);
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < 10; i++) {
+            int n = sc.nextInt();
+            if (max < arr)
+                max = arr;
+        }
+
+        System.out.println(max);
+    }
+}
