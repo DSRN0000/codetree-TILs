df0ce3a5e0c30842e0c3c541bb4e0eda52c76504
DSRN0000 <139203708+DSRN0000@users.noreply.github.com>
2024-10-08 03:08:26 +0900

    Title: 방향에 맞춰 이동, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 241008/방향에 맞춰 이동/move-in-direction.java
+++ 241008/방향에 맞춰 이동/move-in-direction.java
@@ -1,21 +1,23 @@
 import java.util.*;
 public class Main {
+    public static int x, y;
+
+    public static int[] dx = new int[]{1, -1, 0, 0};
+    public static int[] dy = new int[]{0, 0, -1, 1};
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int x = 0;
-        int y = 0;
+
         int n = sc.nextInt();
-        int[] dx = new int[]{1, -1, 0, 0};
-        int[] dy = new int[]{0, 0, -1, 1};
 
         for (int i = 0; i < n; i++) {
             char a = sc.next().charAt(0);
             int num = sc.nextInt();
 
             int dir;
-            if(a == 'E') 
+            if(a == 'E')
                 dir = 0;
-            else if (a == 'w')
+            else if (a == 'W')
                 dir = 1;
             else if (a == 'S')
                 dir = 2;  
