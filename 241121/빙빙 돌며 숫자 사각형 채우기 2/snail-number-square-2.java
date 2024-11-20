baa43f31483c4d31254083b1c62424963178cd04
DSRN <139203708+DSRN0000@users.noreply.github.com>
2024-11-21 01:12:28 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 241121/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
+++ 241121/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
@@ -7,14 +7,14 @@ public class Main {
 	private static int[] dy = new int[] { 0, 1, 0, -1 };
 
 	private static boolean isRange(int x, int y) {
-		return (0 <= x && x < m && 0 <= y && y < n);
+		return (0 <= x && x < n && 0 <= y && y < m);
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
 		m = sc.nextInt();
-		arr = new int[m][n];
+		arr = new int[n][m];
 		arr[x][y] = 1;
 		for (int i = 2; i <= m * n; i++) {
 
@@ -32,8 +32,8 @@ public class Main {
 
 		}
 
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
 				System.out.print(arr[i][j] + " ");
 			}
 			System.out.println();
