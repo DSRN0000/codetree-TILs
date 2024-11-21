9b4d25a8ade2ac2b3e4d93c1be8ff786e5fd9864
DSRN <139203708+DSRN0000@users.noreply.github.com>
2024-11-21 11:16:08 +0900

    Title: 정수 선언하고 곱 출력, Time: 95ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241121/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 26;
+        int b = 5;
+        int mul = a * b;
+        System.out.printf("%d * %d = %d", a, b, mul);
+    }
+}
