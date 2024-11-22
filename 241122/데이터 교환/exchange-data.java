f70871a3dcffc8d1f51b4e942160e96b9dd33468
DSRN <139203708+DSRN0000@users.noreply.github.com>
2024-11-22 10:52:46 +0900

    Title: 데이터 교환, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

--- 241122/데이터 교환/exchange-data.java
+++ 241122/데이터 교환/exchange-data.java
@@ -4,9 +4,13 @@ public class Main {
         int b  = 6;
         int c  = 7;
 
+        int tmp;
+        tmp  = b;
+        int tmp1;
+        tmp1  = c;
         b = a;
-        c = b;
-        a = c;
+        c = tmp;
+        a = tmp1;
 
         System.out.println(a);
         System.out.println(b);
