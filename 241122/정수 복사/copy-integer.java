0dfd7d5042a570a2e6e1afd345700c89575c47d7
DSRN <139203708+DSRN0000@users.noreply.github.com>
2024-11-22 10:48:36 +0900

    Title: 정수 복사, Time: 96ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241122/정수 복사/copy-integer.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        b = a;
+        int mul = a*b;
+
+        System.out.printf("%d %d\n", a, b);
+        System.out.println(mul);
+    }
+}
