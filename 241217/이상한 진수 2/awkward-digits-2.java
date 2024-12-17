607c1a42dbe13f655037dec00ca06a7910b4448f
DSRN <139203708+DSRN0000@users.noreply.github.com>
2024-12-17 16:07:41 +0900

    Title: 이상한 진수 2, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241217/이상한 진수 2/awkward-digits-2.java
+++ 241217/이상한 진수 2/awkward-digits-2.java
@@ -6,17 +6,19 @@ public class Main {
         String s = Integer.toString(a);
 
         int cnt = 0;
-        for (int i = 0; i < s.length() - 1; i++) {
+        for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '0') {
                 cnt += 1;
+                System.out.println(s.substring(0, i));
+                System.out.println(s.substring(i));
                 s = s.substring(0, i) + '1' + s.substring(i + 1);
-
                 break;
             }
         }
         if (cnt == 0) {
             s = s.substring(0, s.length() - 1) + '0';
         }
+
         int result = Integer.parseInt(s, 2);
 
         System.out.println(result);
