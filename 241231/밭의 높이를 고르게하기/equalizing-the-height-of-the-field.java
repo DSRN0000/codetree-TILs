7736c71f125f78991c5ad86c1fbe6a9304029234
DSRN <139203708+DSRN0000@users.noreply.github.com>
2024-12-31 11:29:39 +0900

    Title: 밭의 높이를 고르게하기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 241231/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.java
+++ 241231/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.java
@@ -1,10 +1,11 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int h = sc.nextInt();
-        int t = sc.nextInt();
+        int n = sc.nextInt(); // 밭의 개수
+        int h = sc.nextInt(); // 목표 높이
+        int t = sc.nextInt(); // 최소 연속 구간 길이
         int[] arr = new int[n];
         int[] arr2 = new int[n];
 
@@ -12,49 +13,32 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
-        for (int p = 0; p < n; p++) {
-            arr2[p] = arr[p];
-        }
-
         int answer = Integer.MAX_VALUE;
-        for (int i = 0; i < n - t; i++) {
-            int cnt = 0;
-            int result = 0;
 
-            for (int p = 0; p < n; p++) {
-                arr2[p] = arr[p];
-            }   
+        // 연속 구간의 시작 위치를 설정
+        for (int i = 0; i <= n - t; i++) {
+            // arr2 초기화
+            System.arraycopy(arr, 0, arr2, 0, n);
 
-
-            while(result < t) {
-                for (int j = i; j < i + t; j++) {
-                    if (arr2[j] != h && arr2[j] < h) {
-
-                        arr2[j] += 1;
-
-                        cnt += 1;
-                    }
-
-                    if (arr2[j] != h && arr2[j] > h) {
-
-                        arr2[j] -= 1;
-
-                        cnt += 1;
-                    }
-
-                    if (arr2[j] == h) {
-                        result += 1;
-
-                    }
-
-                    if (result == t) {
-
-                        break;
-                    }
+            int cnt = 0; // 비용
+            // i부터 i + t 구간을 목표 높이 h로 맞추기
+            for (int j = i; j < i + t; j++) {
+                // 현재 높이가 목표 높이보다 낮은 경우
+                if (arr2[j] < h) {
+                    cnt += h - arr2[j];
+                    arr2[j] = h;
+                }
+                // 현재 높이가 목표 높이보다 높은 경우
+                else if (arr2[j] > h) {
+                    cnt += arr2[j] - h;
+                    arr2[j] = h;
                 }
             }
+
+            // 최소 비용 갱신
             answer = Math.min(answer, cnt);
         }
+
         System.out.println(answer);
     }
 }
