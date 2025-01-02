5bd03a09f4f916dcbaaf8c2fd7c974055db60bae
DSRN <139203708+DSRN0000@users.noreply.github.com>
2025-01-02 23:13:09 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 250102/가장 가까운 두 점 사이의 거리/closest-pair-of-points.java
+++ 250102/가장 가까운 두 점 사이의 거리/closest-pair-of-points.java
@@ -2,7 +2,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int answer = 0;
         int n = sc.nextInt();
         int[] x = new int[n];
         int[] y = new int[n];
@@ -12,15 +11,13 @@ public class Main {
             y[i] = sc.nextInt();
         }
 
-        int minx = Integer.MAX_VALUE;
-        int miny = Integer.MAX_VALUE;
+        int answer = Integer.MAX_VALUE;
         for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
-                minx = Math.min(minx, Math.abs(x[i] - x[j]));
-                miny = Math.min(miny, Math.abs(y[i] - y[j]));
+                int result = (x[i] - x[j]) * (x[i] - x[j]) + ((y[i] - y[j]) * (y[i] - y[j]));
+                answer = Math.min(answer, result);
             }
         }
-        answer = (minx * minx) + (miny * miny);
         System.out.println(answer);
     }
 }
