7f6daa0f32faaffdfa51f4159396b5e82ada26f5
DSRN <139203708+DSRN0000@users.noreply.github.com>
2025-01-07 21:24:54 +0900

    Title: 숫자들의 최대 차, Time: 305ms, Memory: 13MB, Status: Passed - CodeTree

--- 250107/숫자들의 최대 차/maximum-difference-in-numbers.java
+++ 250107/숫자들의 최대 차/maximum-difference-in-numbers.java
@@ -1,32 +1,35 @@
-import java.util.*;
-public class Main {
-    private static int n, k;
-    private static int[] arr;
+import java.util.Scanner;
 
-    private static int countNum(int l, int r) {
+public class Main {
+    public static final int MAX_N = 1000;
+    public static final int MAX_NUM = 10000;
+
+    public static int n, k;
+    public static int[] arr = new int[MAX_N];
+
+    public static int countNum(int l, int r) {
         int cnt = 0;
-        for(int i = 0; i < n; i++) {
-            if(l <= arr[i] && arr[i] <= r) {
+        for(int i = 0; i < n; i++)
+            if(l <= arr[i] && arr[i] <= r)
                 cnt++;
-            }
-        }
+
         return cnt;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         n = sc.nextInt();
         k = sc.nextInt();
-        arr = new int[n];
-
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
+
+        int ans = 0;
+
+        for(int i = 1; i <= MAX_NUM; i++) {
+            ans = Math.max(ans, countNum(i, i + k));
         }
 
-        int answer = 0;
-        for (int i = 1; i < n; i++) {
-            answer = Math.max(answer, countNum(i, i + k));
-        }
-        System.out.println(answer);
+        System.out.println(ans);
     }
 }
