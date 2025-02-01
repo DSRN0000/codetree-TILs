ddf4e18f0e4dcda96d68af71277a5cf081897760
DSRN <139203708+DSRN0000@users.noreply.github.com>
2025-02-01 15:36:52 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 1383ms, Memory: 32MB, Status: Passed - CodeTree

---
+++ 250201/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Main {
+    private static int k, n;
+    private static ArrayList<Integer> arr = new ArrayList<>();
+
+    private static void print() {
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.print(arr.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    private static void loop(int number) {
+        if (n == number) {
+            print();
+            return;
+        }
+
+        for (int i = 1; i <= k; i++) {
+            arr.add(i);
+            loop(number + 1);
+            arr.remove(arr.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        k = sc.nextInt();
+        n = sc.nextInt();
+
+        loop(0);
+    }
+}
