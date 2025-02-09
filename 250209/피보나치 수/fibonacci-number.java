c2d4b4369d75171f4ca8f4da730a4705db31ef3b
DSRN <139203708+DSRN0000@users.noreply.github.com>
2025-02-09 21:07:29 +0900

    Title: 피보나치 수, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 250209/피보나치 수/fibonacci-number.java
+++ 250209/피보나치 수/fibonacci-number.java
@@ -1,18 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
+    public static final int MAX_NUM = 45;
+
+    public static int n;
+    public static int[] dp = new int[MAX_NUM + 1];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        int[] dp = new int[n + 1];
+        n = sc.nextInt();
+
         dp[1] = 1;
         dp[2] = 1;
 
-        if (n != 1 && n != 2) {
-            for (int i = 3; i <= n; i++) {
-                dp[i] = dp[i - 1] + dp[i - 2];
-            } 
-        }
+        for(int i = 3; i <= n; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
         
         System.out.println(dp[n]);
     }
