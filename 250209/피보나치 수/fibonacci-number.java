3827c813694c3c963d3985d842b274b97b6b5839
DSRN <139203708+DSRN0000@users.noreply.github.com>
2025-02-09 21:09:46 +0900

    Title: 피보나치 수, Time: 129ms, Memory: 10MB, Status: Runtime Error - CodeTree

--- 250209/피보나치 수/fibonacci-number.java
+++ 250209/피보나치 수/fibonacci-number.java
@@ -1,22 +1,16 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class Main {
-    public static final int MAX_NUM = 45;
-
-    public static int n;
-    public static int[] dp = new int[MAX_NUM + 1];
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        n = sc.nextInt();
+        int n = sc.nextInt();
+        int[] dp = new int[n + 1];
 
         dp[1] = 1;
         dp[2] = 1;
 
-        for(int i = 3; i <= n; i++)
+        for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
-        
+        } 
         System.out.println(dp[n]);
     }
 }
