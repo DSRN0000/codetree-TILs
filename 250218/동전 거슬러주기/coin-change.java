8bf26572bfa60c9323bd29cd871a96f9d6bc7de5
DSRN <139203708+DSRN0000@users.noreply.github.com>
2025-02-18 16:27:20 +0900

    Title: 동전 거슬러주기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250218/동전 거슬러주기/coin-change.java
+++ 250218/동전 거슬러주기/coin-change.java
@@ -9,28 +9,23 @@ public class Main {
             dp[i] = Integer.MAX_VALUE;
         }
         dp[0] = 0;
-        arr[0] = 0;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
-        arr = new int[n + 1];
+        arr = new int[n];
         dp = new int[m + 1];
 
         initalize();
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
         for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
+            for (int j = 0; j < n; j++) {
                 if (i >= arr[j]) {
-                    if (dp[i - arr[j]] == Integer.MAX_VALUE) {
-                        continue;
-                    }
-
                     dp[i] = Math.min(dp[i], dp[i - arr[j]] + 1);
                 }
             }
